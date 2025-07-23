9ac4da7a90621a20584fe707336ae52177986a78
Rookie3d <rocmdes@gmail.com>
2025-07-23 12:39:11 +0200

    Update estimate_camera.py

--- scripts/estimate_camera.py
+++ scripts/estimate_camera.py
@@ -8,14 +8,15 @@ import numpy as np
 from glob import glob
 from pycocotools import mask as masktool
 
-from lib.pipeline import video2frames, detect_segment_track, visualize_tram
+from lib.pipeline import video2frames, detect_segment_track
 from lib.camera import run_metric_slam, calibrate_intrinsics, align_cam_to_world
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--video", type=str, default='./example_video.mov', help='input video')
+parser.add_argument("--img_folder", type=str, default='./example_video.mov', help='input folder')
 parser.add_argument("--static_camera", action='store_true', help='whether the camera is static')
 parser.add_argument("--visualize_mask", action='store_true', help='save deva vos for visualization')
+parser.add_argument("--output_folder", type=str, default='./example_video.mov', help='output')
 args = parser.parse_args()
 
 # File and folders
@@ -23,18 +24,18 @@ file = args.video
 root = os.path.dirname(file)
 seq = os.path.basename(file).split('.')[0]
 
-seq_folder = f'results/{seq}'
-img_folder = f'{seq_folder}/images'
+seq_folder = args.output_folder
+img_folder = args.img_folder
 os.makedirs(seq_folder, exist_ok=True)
 os.makedirs(img_folder, exist_ok=True)
 
 ##### Extract Frames #####
-print('Extracting frames ...')
-nframes = video2frames(file, img_folder)
+#print('Extracting frames ...')
+#nframes = video2frames(file, img_folder)
 
 ##### Detection + SAM + DEVA-Track-Anything #####
 print('Detect, Segment, and Track ...')
-imgfiles = sorted(glob(f'{img_folder}/*.jpg'))
+imgfiles = sorted(glob(f'{img_folder}/*.jpg')+glob(f'{img_folder}/*.png'))
 boxes_, masks_, tracks_ = detect_segment_track(imgfiles, seq_folder, thresh=0.25, 
                                                min_size=100, save_vos=args.visualize_mask)
 
